ec4891fe9bc23cb89a490baa6059566b502b2c06
Javier Carmona <javikarmona@gmail.com>
2019-11-24 16:02:25 +0100

    first steps with index.html

--- app.py
+++ app.py
@@ -7,11 +7,12 @@ Created on Sun Nov 24 14:40:52 2019
 """
 
 from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return "Welcome!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
