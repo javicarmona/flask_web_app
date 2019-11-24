499321329835d7b797d9c7b7a977dacc93bd7f41
Javier Carmona <javikarmona@gmail.com>
2019-11-24 16:22:34 +0100

    uploading template that works

--- app.py
+++ app.py
@@ -12,7 +12,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    titulo = "GeekyFlask"
+    usuario = {'nombre': 'Alejandro'}
+    return render_template('index.html',
+                           titulo=titulo,
+                           usuario=usuario)#render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
